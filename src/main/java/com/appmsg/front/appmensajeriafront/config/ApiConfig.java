1116853537c6342ce23497556e81a51d7c259aee
goyo <gregorio.escriche.dosa@gmail.com>
2026-04-26 16:35:04 +0200

    Primer login redirect a la configuración Configuración por defecto cargada Al crear el usuario debe guardarse también una configuración por defecto Añadir más variedad de temas / libreria de tema Poner foto de perfil propia

--- src/main/java/com/appmsg/front/appmensajeriafront/config/ApiConfig.java
+++ src/main/java/com/appmsg/front/appmensajeriafront/config/ApiConfig.java
@@ -2,6 +2,5 @@ package com.appmsg.front.appmensajeriafront.config;
 
 public class ApiConfig {
 //    public static final String BASE_API_URL = "http://localhost:8080";
-//    public static final String BASE_API_URL = "http://localhost:8080/APPMensajeriaUEM_war_exploded";
-    public static final String BASE_API_URL = "https://appmensajeriauem.onrender.com";
+    public static final String BASE_API_URL = "http://localhost:8080/APPMensajeriaUEM_war_exploded";
 }
